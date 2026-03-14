58a9fcde0bc1d836e491c96b1adbafbe62573e42
SeditioCMS <bilgi@ntka.org>
2026-03-14 09:14:45 +0300

    Add 'thanks' plugin and UI/template fixes
    Introduce the new 'thanks' plugin (multiple PHP, lang, tpl and admin files) and add several UI/template assets (contact, news, passrecover, polls.poll, page.print). Fix forum behavior (maingrp fallback, date mask for en/tr), adjust forum/topic/post templates to show thanks/updated info and layout refinements. Improve language file loading safety (adminqv, comments), streamline comments plugin logic and traversal bug fixes, add thanks counters to user details, tweak page listing (category menu and data attributes) and switch poll template usage. Misc minor cleanups (skinfile references, whitespace/newline fixes) and a TODO note in pm functions for future PM-sending helper.

--- system/functions.admin.php
+++ system/functions.admin.php
@@ -358,10 +358,10 @@ function sed_loadconfigmap()
 	$result[] = array('index', '02', 'homemetadescription', 1, '', ''); // ---- New in v179
 	$result[] = array('index', '03', 'homemetakeywords', 1, '', ''); // ---- New in v179
 
-	$result[] = array('time', '11', 'dateformat', 1, 'Y-m-d H:i', '');
-	$result[] = array('time', '11', 'formatmonthday', 1, 'm-d', '');
-	$result[] = array('time', '11', 'formatyearmonthday', 1, 'Y-m-d', '');
-	$result[] = array('time', '11', 'formatmonthdayhourmin', 1, 'm-d H:i', '');
+	$result[] = array('time', '11', 'dateformat', 1, 'd.m.Y H:i', '');
+	$result[] = array('time', '11', 'formatmonthday', 1, 'd.m', '');
+	$result[] = array('time', '11', 'formatyearmonthday', 1, 'd.m.Y', '');
+	$result[] = array('time', '11', 'formatmonthdayhourmin', 1, 'd.m H:i', '');
 	$result[] = array('time', '11', 'servertimezone', 1, '0', '');
 	$result[] = array('time', '12', 'defaulttimezone', 1, '0', '');
 	$result[] = array('time', '14', 'timedout', 2, '1200', array(30, 60, 120, 300, 600, 900, 1200, 1800, 2400, 3600));
@@ -606,8 +606,7 @@ function sed_plugin_install($pl)
 		$res .= "<strong>Looking for configuration entries in the setup file...</strong> ";
 
 		/* ===== */
-		$path_lang_setup = SED_ROOT . "/plugins/" . $pl . "/lang/" . $pl . "." . $cfg['defaultlang'] . ".lang.php";
-		if (file_exists($path_lang_setup)) {
+		if ($path_lang_setup = sed_langfile($pl, 'plugin', $cfg['defaultlang'])) {
 			require($path_lang_setup);
 		}
 		/* ===== */
